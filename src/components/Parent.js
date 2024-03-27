fecd743d10c7026e3c9ab973c27eb2b34383c35f
Edison Zhuang <edisen.zhuang@gmail.com>
2024-03-28 02:13:05 +0800

    added code explanation

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -6,9 +6,17 @@ const Parent = () => {
     const [data, setData] = useState({n:1});
 
     /*
-        This method will set "data" state to either 1 or 2.
-        The "data" is passed to the <Child /> component as prop.
-        Since object is passed by reference, whenever object is passed as prop to child component, child will re-render.
+        Code Explanation:
+        - The "handleRandomNumber" method will set "data" state to either 1 or 2.
+        - The "data" is passed to the <Child /> component as prop.
+        - Since object is passed by reference, whenever object is passed as prop to child component, child will re-render.
+
+        Problem: If the new random number is the same as the previous number, child component should not render.
+        - See child component comments on how to detect re-renders.
+
+        To solve: Utilize the useMemo hooks to prevent child component from re-rendering if the newly generated random number
+        is the same as the previous. In another words, the child component should re-render only if the newly generate random
+        number is not the same as the previous state.
     */
     const handleRandomNumber = () => {
         setData(Math.floor(Math.random() * 2)+1 === 1 ? {n:1} : {n:2});
